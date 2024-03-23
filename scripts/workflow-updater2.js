97770cab2e88ed1bb9b0d513a26199d2b54d86e2
Mickey Gousset <mickeygousset@github.com>
2024-03-23 13:34:49 -0500

    getting list of all workflows in org

---
+++ scripts/workflow-updater2.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const { Octokit } = require("@octokit/rest");
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
+
+const octokit = new Octokit({ auth: process.env.WORKFLOW_GITHUB_TOKEN });
+
+const org = 'devopselvis'; // Replace with your organization name
+
+async function getWorkflowUrls() {
+    const repos = await octokit.repos.listForOrg({
+        org
+    });
+
+    let workflowUrls = [];
+
+    for (let repo of repos.data) {
+        const workflows = await octokit.actions.listRepoWorkflows({
+            owner: org,
+            repo: repo.name
+        });
+
+        for (let workflow of workflows.data.workflows) {
+            const workflowUrl = `https://github.com/${org}/${repo.name}/actions/workflows/${workflow.path}`;
+            workflowUrls.push(workflowUrl);
+        }
+    }
+
+    return workflowUrls;
+}
+
+let workflowUrls = await getWorkflowUrls();
+
+// Output workflow URL to command line
+getWorkflowUrls().then(workflowUrls => {
+  console.log(workflowUrls.join('\n'));
+}).catch(error => {
+  console.error(error);
+});
